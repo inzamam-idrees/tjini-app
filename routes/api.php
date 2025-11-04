a3719ba917807e0fd7d85e7a335db9846125520a
Inzamam Idrees <inziprince87@gmail.com>
2025-11-04 12:31:48 -0800

    Get User API is done

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\NotificationController;
+use App\Models\User;
 
 // Public routes for registration and login
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
-
+Route::get('/user/{id}', function ($id) {
+    return User::find($id);
+});
 // Protected route for logout (requires authentication)
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
@@ -18,6 +21,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     // Unified notification endpoint for parent and dispatcher actions
     Route::post('/notify/users', [NotificationController::class, 'notifyUsers']);
+    Route::get('/notifications', [NotificationController::class, 'listNotifications']);
 });
 
 Route::get('/test', function (Request $request) {
